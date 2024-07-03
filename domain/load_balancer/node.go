98bbd0f611f281bc06a45a463a64e5e167082f3a
GyoWan <rydhkstptkd@naver.com>
2024-07-03 19:14:13 +0900

    feat: domain model 정의

--- domain/load_balancer/node.go
+++ domain/load_balancer/node.go
@@ -1,1 +1,24 @@
 package load_balancer
+
+import (
+	"sync"
+	"time"
+)
+
+type Node struct {
+	// 노드 주소
+	Address string
+	// 분당 HTTP 바이트 수 제한
+	BPM int32
+	// 현 노드에서 사용한 총 바이트량
+	UsedByteSize int32
+	// 분당 요청 수 제한
+	RPM int32
+	// 현 노드에 들어온 총 요청 수량
+	RequestCounts int32
+	// 해당 노드 Rate Limit 초기화 시간 (분당 1번)
+	ResetTime time.Time
+	// 노드가 Heap 에 위치하고 있는 index
+	Index int
+	Mu    sync.Mutex
+}
--- domain/load_balancer/request.go
+++ domain/load_balancer/request.go
@@ -1,1 +1,6 @@
 package load_balancer
+
+type Request struct {
+	// 요청 HTTP Body 크기라고 가정
+	BodySize int32
+}

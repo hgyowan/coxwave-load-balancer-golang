637c455072157051222b718f7aedcb048eefffa7
GyoWan <rydhkstptkd@naver.com>
2024-07-03 19:50:56 +0900

    feat: node heap 구현

--- domain/load_balancer/node.go
+++ domain/load_balancer/node.go
@@ -5,7 +5,49 @@ import (
 	"time"
 )
 
+// NodeHeap
+// 현재 가장 적은 요청을 수행 하고 있는 Node 를 찾기 위한 min heap 구현
+// https://go.dev/src/container/heap/example_pq_test.go 참고 하여 heap interface 구현
 type NodeHeap []*Node
+
+func (nh NodeHeap) Len() int { return len(nh) }
+
+// Less
+// 적은 요청량 일수록 우선순위 높음
+func (nh NodeHeap) Less(i, j int) bool {
+	return nh[i].RequestCounts < nh[j].RequestCounts
+}
+
+// Swap
+// 값, 인덱스 순서 변경
+func (nh NodeHeap) Swap(i, j int) {
+	nh[i], nh[j] = nh[j], nh[i]
+	nh[i].Index = i
+	nh[j].Index = j
+}
+
+// Push
+// 기존 heap 맨 뒤에 새로운 값 추가
+// 이후 내부적으로 up 함수 수행
+func (nh *NodeHeap) Push(x interface{}) {
+	n := len(*nh)
+	item := x.(*Node)
+	item.Index = n
+	*nh = append(*nh, item)
+}
+
+// Pop
+// heap 의 마지막 값 추출 후 heap 길이 감소
+// 이후 내부적으로 down 함수 수행
+func (nh *NodeHeap) Pop() interface{} {
+	old := *nh
+	n := len(old)
+	item := old[n-1]
+	old[n-1] = nil
+	item.Index = -1
+	*nh = old[0 : n-1]
+	return item
+}
 
 type Node struct {
 	// 노드 주소

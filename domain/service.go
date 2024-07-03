660e28f6b009f45db1090b205b6d571eb18d4892
GyoWan <rydhkstptkd@naver.com>
2024-07-03 19:24:53 +0900

    feat: load balancer interface 추가

--- domain/service.go
+++ domain/service.go
@@ -1,1 +1,7 @@
 package domain
+
+import "coxwave-load-balancer-golang/domain/load_balancer"
+
+type LoadBalancerService interface {
+	Route(r *load_balancer.Request)
+}
---
+++ go.mod
@@ -0,0 +1,3 @@
+module coxwave-load-balancer-golang
+
+go 1.16
